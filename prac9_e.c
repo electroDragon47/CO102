5c265d5320cfd480e239adbfa8ad742b5b5dbc30
Electro Dragon <92241157+electroDragon47@users.noreply.github.com>
2026-02-10 20:32:42 +0530

    change in 5th of 9

--- prac9_e.c
+++ prac9_e.c
@@ -2,12 +2,16 @@
 
 int main()
 {
-    int i, j;
+    int i, j, space;
 
     for(i = 1; i <= 5; i++)
     {
+        for(space = 1; space <= 5 - i; space++)
+            printf("  ");
+
         for(j = 1; j <= i; j++)
             printf("%d ", j);
+
         printf("\n");
     }
 
